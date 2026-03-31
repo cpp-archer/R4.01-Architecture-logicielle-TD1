5b389906229bd47edffaa44d7a60d4de1ca8f41a
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-03-31 06:14:47 +0200

    a supp apres

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Fichier de configuration
+ * Le fichier config.php ne doit JAMAIS être versionné
+ */
+
+// Configuration de la base de données
+define('DB_HOST', 'mysql');
+define('DB_USER', 'admin');
+define('DB_PASS', 'password');
+define('DB_NAME', 'annonces_db');
+
+?>
